b2a2c3846e45dc15cecf48951633024c66e8daa3
cod-star <1335564743@qq.com>
2020-10-01 20:14:10 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,6 @@
 // pages/home/home.js
+import request from '../../service/network.js'
+
 Page({
 
   /**
@@ -12,6 +14,30 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 发送网络请求
+    // 1. 原生的
+    // wx.request({
+    //   url: 'url',
+    //   data: {
+    //     // get请求，但是携带请求参数
+    //     type:"sell",
+    //     page: 1
+    //   },
+    //   success: function(res) {
+    //   }
+    // })
+    // post请求，并且携带参数
+
+    // 2. 使用封装的
+    // promise最大的好处是为了防止出现回调函数
+    request({
+      url: '',
+    }).then(res => {
+      // console.log(res);
+    }).catch(err => {
+      // console.log(err);
+    })
+
 
   },
 
