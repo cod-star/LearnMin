839da43dce9b3b6610b4e5a0f367981449c790ff
cod-star <1335564743@qq.com>
2020-10-01 21:00:14 +0800

    弹窗

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,58 @@
 // pages/home/home.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast() {
+    wx.showToast({
+      title: '吼吼吼',
+      duration: 3000,
+      icon: 'loading',
+      mask: true
+    })
+  },
+  handleShowModal() {
+    wx.showModal({
+      title: '我的model',
+      content: '我是内容',
+      // showCancel:false,
+      cancelText: '退出',
+      success: function(res) {
+        console.log(res);
+        if(res.cancel) {
+          console.log('取消按钮');
+        }
+        if(res.confirm) {
+          console.log('确定按钮');
+        }
+      }
+    })
+  },
+  handleShowLoading() {
+    wx.showLoading({
+      title: '加载',
+      mask: true
+    })
+    setTimeout(() => {
+      // 必须手动hideLoading才会让loading消失
+      wx.hideLoading()
+    },1000)
+  },
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      itemColor: 'red',
+      success: function(res){
+        console.log(res.tapIndex);
+        
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(options){
+    return {
+      title: '你好',
+      path: '/pages/about/about',
+      imageUrl: ''
+    }
   }
+  
 })
