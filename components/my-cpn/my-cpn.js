4c5be3c12ec159a1fb75e5e382316c2e85c5e17d
cod-star <1335564743@qq.com>
2020-10-01 16:16:10 +0800

    组件

---
+++ components/my-cpn/my-cpn.js
@@ -0,0 +1,7 @@
+// components/my-cpn/my-cpn.js
+Component({
+  // 组件样式之间的相互影响
+  options: {
+    styleIsolation: "shared"
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
+    count: 0
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleInea(){
+    // console.log('00000');
+    this.setData({
+      count: this.data.count + 1
+    })
+    
+  },
+  handleTabClick(event) {
+    console.log(event);
+    
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  handleIncr(){
+    // 通过组件对象来修改
+    const my_sel = this.selectComponent('.sel')
 
-  },
+    // my_sel.setData({
+    //   count: my_sel.data.count + 20
+    // })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 通过方法修改
+    my_sel.increment(10)
   }
+
+
+  
+
+  
+ 
 })
